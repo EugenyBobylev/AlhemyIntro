4e91a046b6c75d258fd3419cdd3e72d6d44f8b20
Eugeny Bobylev <bea@hotbox.ru>
2020-02-04 22:56:38 +1000

    Уточнены связи между таблицами заработал тестовый сценарий

--- DataBaseTest.py
+++ DataBaseTest.py
@@ -1,5 +1,6 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from models import Person, Order, Base
 
 
@@ -13,3 +14,26 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 connStr = 'sqlite:///' + os.path.join(basedir, 'app.db')
 engine = create_engine(connStr, echo=False)
 recreate_database(engine)
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+person = Person()
+person.name = 'Eugeny Bobylev'
+person.email = 'ebobylev@gmail.com'
+person.phone = '+79247401790'
+person.is_customer = True
+session.add(person)
+
+order = Order()
+order.customer = person
+order.url_source='https//contoso.com'
+session.add(order)
+
+session.commit()
+print(order.customer)
+
+for ord in person.customer_orders:
+    print(ord)
+
+session.close()
--- models.py
+++ models.py
@@ -12,11 +12,11 @@ class Person(Base):
     name = Column("Name", String(128), nullable=False)
     email = Column("Email", String(128))
     phone = Column("Phone", String(64))
-    is_customer = Column("IsCustomer", Boolean)
-    is_performer = Column("IsPerformer", Boolean)
+    is_customer = Column("IsCustomer", Boolean, default=False)
+    is_performer = Column("IsPerformer", Boolean, default=False)
 
-    customer_orders = relationship("Order", lazy='dynamic', back_populates="customer")
-    performer_orders = relationship("Order", lazy='dynamic', back_populates="performer")
+    customer_orders = relationship("Order", foreign_keys='Order.id_customer', back_populates="customer", lazy=False)
+    performer_orders = relationship("Order", foreign_keys='Order.id_performer', back_populates="performer", lazy=False)
 
     def __repr__(self):
         return f'id={self.id}; name={self.name}; email={self.email}; phone={self.phone}'
@@ -31,8 +31,8 @@ class Order(Base):
     url_source = Column("UrlSource", String(255), nullable=False)
     url_result = Column("UrlResult", String(255))
 
-    customer = relationship("Person", back_populates="customer_orders")
-    performer = relationship("Person", back_populates="performer_orders")
+    customer = relationship("Person", foreign_keys=[id_customer])
+    performer = relationship("Person", foreign_keys=[id_performer])
 
     def __repr__(self):
         return f'id={self.id}; customer={self.customer}; performer={self.performer}' \
